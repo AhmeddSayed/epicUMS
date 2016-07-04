364d5e77859fe1960402c714545bfc0edf8c3000
ahmeddsayed <ahmedd.abd.allah@gmail.com>
2016-07-05 00:10:20 +0200

    updated security configurations for ACL roles

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,18 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use \AppBundle\Entity\User;
 
-class DefaultController extends Controller
-{
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+class DefaultController extends Controller {
+
+    public function indexAction(Request $request) {
+        $securityContext = $this->container->get('security.authorization_checker');
+
+        if ($securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            return $this->render('AppBundle:Default:index.html.twig');
+        } else {
+            return $this->render('AppBundle:Security:login.html.twig');
+        }
     }
+
 }
